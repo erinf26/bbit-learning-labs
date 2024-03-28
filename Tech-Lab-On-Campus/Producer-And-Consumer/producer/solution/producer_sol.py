aa9133e349d4091708496b23eee8e86b0373e3b2
Elizabeth Schaefer <elizabethschaefer@MacBook-Pro-109.stdusr.yale.internal>
2024-03-28 18:06:36 -0400

    producer first draft

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -0,0 +1,48 @@
+from producer_interface import mqProducerInterface
+
+import pika
+import os
+
+
+class mqproducer(mqProducerInterface):
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
+        # body of constructor
+        # str ?
+
+        #set up parametes
+        self.routing_key = routing_key
+
+        self.exchange_name = exchange_name
+
+        #call connection
+        self.setupRMQConnection()
+
+
+    def setupRMQConnection(self) -> None:
+        # Set-up Connection to RabbitMQ service
+
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        connection = pika.BlockingConnection(parameters=con_params)
+
+        # Establish Channel
+        self.channel = connection.channel()
+
+        # Create the exchange if not already present
+        self.channel.exchange_declare(exchange=self.exchange_name)
+
+        pass
+
+    def publishOrder(self, message: str) -> None:
+        # Basic Publish to Exchange
+        self.channel.basic_publish(
+            exchange=self.exchange_name,
+            routing_key=self.routing_key,
+            body=message,
+        )
+
+        # Close Channel
+        # Close Connection
+        self.channel.close()
+        self.connection.close()
+    
+        pass
